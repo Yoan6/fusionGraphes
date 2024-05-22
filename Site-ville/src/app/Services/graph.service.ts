1a2afe49c1d61639611153f26656926d787aa08a
delannyo <yoan.delannoy1@gmail.com>
2024-05-22 14:29:46 +0200

    Ajoute du site en Angular

---
+++ Site-ville/src/app/Services/graph.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GraphService {
+
+  // Url du fichier json des données du graphe
+  private url = '../../../../../Fusions/graph_data.json';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getGraphData(): Observable<any> {
+    return this.http.get(this.url);
+  }
+}
